634878d802572c0c3e2f4d874fc2af3cb5e56896
Joao Marques <programador.projeto@gmail.com>
2023-04-10 10:47:55 -0300

    First commit
    First commit

---
+++ main.py
@@ -0,0 +1,22 @@
+from googletrans import Translator
+import streamlit as st
+
+st.set_page_config(
+    page_title="code.py Tradutor",
+    page_icon='codepy.jpeg'
+)
+
+translator = Translator()
+
+st.title('Tradutor Inglês <> Português')
+
+texto = st.text_area('Digite aqui o texto:')
+
+idioma = st.selectbox('Selecione o idioma da tradução:', ['pt', 'en'])
+
+if st.button('Traduzir'):
+    lista = [texto]
+    translations = translator.translate(lista, dest=idioma)
+    for translation in translations:
+        traducao = translation.text
+    st.markdown(traducao)
